1b5f62144a9e26027f23732922e73ab9d08175eb
Moc <tijnkuyper@gmail.com>
2014-08-10 14:00:28 +0200

    Basic template structure (start). Work in progress.

---
+++ templates/oweme_template.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Owe Me! - an e107 plugin by Tijn Kuyper
+ *
+ * Copyright (C) 2013-2014 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+
+$OWEME_TEMPLATE['frontend']= '
+
+
+
+
+
+';
+
+
+??
