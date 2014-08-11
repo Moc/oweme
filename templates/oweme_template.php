ed9b7edbdf3b47b317cd710462eeeab2ee7bf41c
Moc <tijnkuyper@gmail.com>
2014-08-11 20:56:20 +0200

    Updated copyright date

--- templates/oweme_template.php
+++ templates/oweme_template.php
@@ -3,7 +3,7 @@
 /*
  * Owe Me! - an e107 plugin by Tijn Kuyper
  *
- * Copyright (C) 2013-2014 Tijn Kuyper (http://www.tijnkuyper.nl)
+ * Copyright (C) 2014-2015 Tijn Kuyper (http://www.tijnkuyper.nl)
  * Released under the terms and conditions of the
  * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
  *
